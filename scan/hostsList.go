942390b2290d38c9f4cf76938ea36fd7a4ab8b30
vegorov <vegorov@px1.vegorov.ru>
2025-03-30 12:05:20 +0300

    методы hostsList

--- scan/hostsList.go
+++ scan/hostsList.go
@@ -1,13 +1,17 @@
 package scan
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"os"
+	"slices"
 	"sort"
 )
 
 var (
-	ErrExists    = errors.New("Хост уже в списке")
-	ErrNotExists = errors.New("Хост не в списке")
+	ErrExists    = errors.New("хост уже в списке")
+	ErrNotExists = errors.New("хост не в списке")
 )
 
 // Список хостов для сканирования
@@ -24,4 +28,51 @@ func (hl *HostsList) search(host string) (bool, int) {
 		return true, i
 	}
 	return false, -1
+}
+
+// Add добавляет хост в список
+func (hl *HostsList) Add(host string) error {
+	if found, _ := hl.search(host); found {
+		return fmt.Errorf("%w: %s", ErrExists, host)
+	}
+	hl.Hosts = append(hl.Hosts, host)
+	return nil
+}
+
+func (hl *HostsList) Remove(host string) error {
+	if found, i := hl.search(host); found {
+		hl.Hosts = slices.Delete(hl.Hosts, i, i+1)
+		return nil
+	}
+	return fmt.Errorf("%w: %s", ErrNotExists, host)
+}
+
+// Load загружает список хостов из файла
+func (hl *HostsList) Load(hostsFile string) error {
+	f, err := os.Open(hostsFile)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		hl.Hosts = append(hl.Hosts, scanner.Text())
+	}
+
+	return nil
+}
+
+// Save сохраняет список хостов в файл
+func (hl *HostsList) Save(hostsFile string) error {
+	output := ""
+	for _, h := range hl.Hosts {
+		output += fmt.Sprintln(h)
+	}
+
+	return os.WriteFile(hostsFile, []byte(output), 0644)
 }
