dcdfbecd87f08536be5fb4b7594231b975c3e0ee
Vladimir Egorov <vegorov@gmail.com>
2025-04-02 19:36:54 +0300

    команда scan - основной функционал

---
+++ cmd/scan.go
@@ -0,0 +1,85 @@
+/*
+Copyright © 2025 Vladimir Egorov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+	"vegorov.ru/go-cli/pScan/scan"
+)
+
+// scanCmd represents the scan command
+var scanCmd = &cobra.Command{
+	Use:   "scan",
+	Short: "Выполнить сканирование открытых портов хостов",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+
+		ports, err := cmd.Flags().GetIntSlice("ports")
+		if err != nil {
+			return err
+		}
+
+		return scanAction(os.Stdout, hostsFile, ports)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(scanCmd)
+	scanCmd.Flags().IntSliceP("ports", "p", []int{22, 80, 443}, "Порты для сканирования")
+}
+
+func scanAction(out io.Writer, hostsFile string, ports []int) error {
+	hl := &scan.HostsList{}
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	results := scan.Run(hl, ports)
+	return printResults(out, results)
+}
+
+func printResults(out io.Writer, results []scan.Results) error {
+	message := ""
+	for _, r := range results {
+		message += fmt.Sprintf("%s\n", r.Host)
+
+		if r.NotFound {
+			message += "Хост не нейден\n\n"
+		}
+		message += fmt.Sprintln()
+
+		for _, p := range r.PortStates {
+			message += fmt.Sprintf("\t%d: %s\n", p.Port, p.Open)
+		}
+		message += fmt.Sprintln()
+	}
+
+	_, err := fmt.Fprint(out, message)
+	return err
+}
